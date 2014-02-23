673c672d421b39aacbf5beb6870ad8f3e99997a1
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-02-22 17:42:58 -0800

    Set development version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ankha "0.1.1"
+(defproject ankha "0.1.2-SNAPSHOT"
   :description "A generic data inspection component for use with Om."
   :url "https://github.com/noprompt/ankha"
   :license {:name "Eclipse Public License"
